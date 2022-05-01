ee532c8aee0faba2964c5e2a89b42306f9f49a44
juan1996buri <juan98buri@gmail.com>
2022-04-30 21:51:23 -0500

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ProductList from './components/ProductList';
+import Register from './components/Register';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import Product from './pages/Product';
+
+const App = () => {
+  return <ProductList />;
+};
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,111 @@
+import IMG from './images/fondo.png';
+import CATEGORY from './images/category2.jpg';
+export const sliderItems = [
+  {
+    id: 1,
+    img: IMG,
+    title: 'title 1',
+    desc: 'En la lección anterior, ',
+    bg: 'f5fafd',
+  },
+  {
+    id: 2,
+    img: IMG,
+    title: 'title 2',
+    desc: 'En la lección anterior, ',
+    bg: 'f5fafd',
+  },
+  {
+    id: 3,
+    img: IMG,
+    title: 'title 3',
+    desc: 'En la lección anterior, ',
+
+    bg: 'f5fafd',
+  },
+  {
+    id: 4,
+    img: IMG,
+    title: 'title 4',
+    desc: 'desc 4',
+    bg: 'f5fafd',
+  },
+  {
+    id: 5,
+    img: IMG,
+    title: 'title 5',
+    desc: 'desc 5',
+    bg: 'f5fafd',
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: CATEGORY,
+    title: 'title 1',
+  },
+  {
+    id: 2,
+    img: CATEGORY,
+    title: 'title 2',
+  },
+  {
+    id: 3,
+    img: CATEGORY,
+    title: 'title 3',
+  },
+  {
+    id: 4,
+    img: CATEGORY,
+    title: 'title 4',
+  },
+  {
+    id: 5,
+    img: CATEGORY,
+    title: 'title 5',
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: CATEGORY,
+  },
+  {
+    id: 2,
+    img: CATEGORY,
+  },
+  {
+    id: 3,
+    img: CATEGORY,
+  },
+  {
+    id: 4,
+    img: CATEGORY,
+  },
+  {
+    id: 5,
+    img: CATEGORY,
+  },
+  {
+    id: 6,
+    img: CATEGORY,
+  },
+  {
+    id: 7,
+    img: CATEGORY,
+  },
+  {
+    id: 8,
+    img: CATEGORY,
+  },
+  {
+    id: 9,
+    img: CATEGORY,
+  },
+  {
+    id: 10,
+    img: CATEGORY,
+  },
+];
